7dfd2a85454dbf4abc4cab6eefd3c5981cdecbd8
tech_e <chris.durfee@dentalqore.com>
2024-12-28 19:16:49 -0700

    Bump version to 0.0.16, add Tailwind CSS configuration, and remove unused modal CSS

--- src/ui.js
+++ src/ui.js
@@ -4,4 +4,5 @@ export * from "./components/molecules/molecules.js";
 export * from "./components/organisms/organisms.js";
 export * from "./components/pages/pages.js";
 export * from "./components/pages/templates/templates.js";
+import "./styles.css";
 
